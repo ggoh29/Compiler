a6a05dd77cb08faed5c84e11d1e3d7b9fc356999
ggoh29 <56758342+ggoh29@users.noreply.github.com>
2020-08-20 21:05:50 +0800

    Update CompilerMain.java

--- CompilerMain.java
+++ CompilerMain.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 public class CompilerMain {
 
     public static void main(String[] args) throws Exception {
-//        Scanner sc = new Scanner(System.in);
-//        String inputStr = sc.nextLine();
-        String inputStr = ("(9 + 5) * 3! * (5 + cos(3*0!)! * cos2*0!) * 5");
+       Scanner sc = new Scanner(System.in);
+       String inputStr = sc.nextLine();
         int length = inputStr.length();
         char[] charArray = inputStr.toCharArray();
         Lexer lex = new Lexer(charArray, length);
